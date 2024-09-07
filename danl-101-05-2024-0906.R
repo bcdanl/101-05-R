e677d58ce679cf381483c3523f7772b2f9727f4b
bcdanl <bchoe@geneseo.edu>
2024-09-07 01:36:27 +0000

    lec05

---
+++ danl-101-05-2024-0906.R
@@ -0,0 +1,37 @@
+
+# R basic -----------------------------------------------------------------
+
+a <- 1
+
+
+# R package ---------------------------------------------------------------
+
+
+# install.packages("tidyverse")
+
+# library() loads R package
+library(tidyverse)
+mpg  # mpg is provided by ggplot2 package
+
+
+
+
+mpg
+mpg
+
+mpg
+mpg
+mpg
+mpg
+mpg
+mpg
+
+
+
+
+# assignment  -------------------------------------------------------------
+
+
+x <- 2
+x < - 3
+
